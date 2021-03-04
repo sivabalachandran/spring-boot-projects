e6c1e60ff64c5da43cfb3d85bd563b5eb241c09b
Siva Balachandran <sivamech@gmail.com>
2021-03-03 21:36:08 -0500

    Updated swagger documentation

--- person/src/main/java/com/ebi/assessment/person/PersonController.java
+++ person/src/main/java/com/ebi/assessment/person/PersonController.java
@@ -2,6 +2,9 @@ package com.ebi.assessment.person;
 
 import com.ebi.assessment.person.persistance.PersonEntity;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -20,6 +23,12 @@ public class PersonController {
 
     private final PersonService personService;
 
+    @ApiOperation(value = "Retrieve a person by Id",
+            response = PersonDto.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+            @ApiResponse(code = 404, message = "Enity not found"),
+    })
     @GetMapping("/persons/{id}")
     public ResponseEntity<PersonDto> findBy(@PathVariable(value = "id", required = true) Integer id) {
 
@@ -31,12 +40,26 @@ public class PersonController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ApiOperation(value = "Retrieve all available entities",
+            response = PersonDto.class,
+            responseContainer = "Set")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+    })
     @GetMapping("/persons")
     public ResponseEntity<Set<PersonDto>> findAll() {
         final var personEntities = personService.findAll();
         return new ResponseEntity<>(convertEntities(personEntities), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Insert list of person entities",
+            notes = "Takes list of person objects and insert it and returns the successful persisted entity values",
+            response = PersonDto.class,
+            responseContainer = "Set")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+            @ApiResponse(code = 500, message = "Invalid server error"),
+    })
     @PostMapping("/persons")
     public ResponseEntity<Set<PersonDto>> create(@RequestBody List<PersonDto> persons) {
         Assert.notEmpty(persons, "persons collection cannot be empty with create API");
@@ -52,6 +75,14 @@ public class PersonController {
         }
     }
 
+    @ApiOperation(value = "Delete list of person entities",
+            notes = "Takes list of person Ids to be deleted and deletes it and returns the successful deleted entity ids",
+            response = Integer.class,
+            responseContainer = "Set")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+            @ApiResponse(code = 500, message = "Invalid server error"),
+    })
     @DeleteMapping("/persons")
     public ResponseEntity<Set<Integer>> delete(@RequestBody Set<Integer> idsToDelete) {
         Assert.notEmpty(idsToDelete, "entity id cannot be empty with delete API");
@@ -67,6 +98,13 @@ public class PersonController {
         }
     }
 
+    @ApiOperation(value = "Update a person by Id",
+            notes = "Takes a map of field name and value to be updated. Api does not discriminate between null and new value. Returns Id if successful",
+            response = Integer.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+            @ApiResponse(code = 500, message = "Invalid server error"),
+    })
     @PatchMapping("/persons/{id}")
     public ResponseEntity<Integer> patch(@PathVariable final Integer id, @RequestBody Map<String, Object> changes) {
         Assert.notEmpty(changes, "changes collection cannot be empty with update API");
@@ -79,6 +117,14 @@ public class PersonController {
         }
     }
 
+    @ApiOperation(value = "Updated list of person entities",
+            notes = "Takes a map of Id and a map of field name and value to be updated. Api does not discriminate between null and new value. Returns Id if successful",
+            response = Integer.class,
+            responseContainer = "Set")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Invalid request"),
+            @ApiResponse(code = 500, message = "Invalid server error"),
+    })
     @PatchMapping("/persons/")
     public ResponseEntity<Set<Integer>> patch(@RequestBody Map<Integer, Map<String, Object>> changes) {
         Assert.notEmpty(changes, "changes collection cannot be empty with update API");
