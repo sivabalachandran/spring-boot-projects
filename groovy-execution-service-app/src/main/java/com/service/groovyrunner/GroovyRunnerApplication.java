a40c3fc8d7612cfb632626f4174ba39c4667dca3
Siva Balachandran <sivamech@gmail.com>
2021-03-15 21:55:12 -0400

    Initial draft for groovy runner

---
+++ groovy-execution-service-app/src/main/java/com/service/groovyrunner/GroovyRunnerApplication.java
@@ -0,0 +1,13 @@
+package com.service.groovyrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovyRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovyRunnerApplication.class, args);
+	}
+
+}
---
+++ groovy-execution-service-app/src/test/java/com/service/groovyrunner/GroovyRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.groovyrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroovyRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
